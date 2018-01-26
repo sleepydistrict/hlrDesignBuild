429e071516e31bf0d61089a623b79886ff250965
Gregory Lesky <sleepydistrict01@gmail.com>
2018-01-26 14:10:17 -0700

    Contact form handeling change

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/navbar/Navbar';
+import { Header, Card, Form, TextArea, Button } from 'semantic-ui-react';
+import './Contact.css';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(event.target.value)
+  }
 
   render() {
 
     return (
-      <div style={{ backgroundColor:'black', backgroundSize: 'cover', backgroundPosition: 'center center', position: 'fixed', width: '100%', top: '0', bottom: '0' }}>
+      <div id='contact'>
+        <div className='Background'>
+        </div>
         <Navbar />
         {/* <Navbar {this.state.activeItem} /> */}
+        <Header
+          as='h1'
+          content='CONTACT H.L. ROGERS DESIGN/BUILD'
+          style={{ fontWeight: 'normal', marginBottom: 0, color: 'orange' }}
+        />
+        <Card fluid>
+          <Card.Content>
+            <Form>
+              <Form.Group unstackable widths={3}>
+              <Form.Input type='text' label='First Name' name="firstName" onChange={this.handleChange} value={this.state.firstName}/>
+              <Form.Input type='text' label='Last Name' name="lastName" onChange={this.handleChange} value={this.state.lastName}/>
+              <Form.Input type='text' label='Email' name="email" onChange={this.handleChange} value={this.state.email}/>
+              </Form.Group>  
+              <Form.Field control={TextArea} label='Message' placeholder='Tell us more about you and how H.L.Rogers Design/Build can help you.' />
+              <Button type='submit' color='orange'>Submit</Button>
+              {/* <Form.Field color='orange' control={Button}>Submit</Form.Field> */}
+            </Form>
+          </Card.Content>
+        </Card>  
       </div> 
     )
   }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../logo.svg';
 import './Home.css';
-import { Header, Button, Grid, Segment, Image } from 'semantic-ui-react';
+import { Header, Button, Grid, Segment, Image, Icon } from 'semantic-ui-react';
 
 class Home extends Component {
+
   render() {
     return (
       <div className="App">
@@ -15,18 +16,23 @@ class Home extends Component {
           content='H.L. ROGERS DESIGN/BUILD'
           style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, color: 'orange' }}
         />
+        <Header 
+        as='h1'
+        inverted
+        content='Where design/build meets sustainable thoughtful projects'
+        />
         <Image src={logo} className="App-logo" alt="logo" centered />
-        <Segment style={{ padding: '30em 0em' }} vertical>
+        <Segment style={{ padding: '19em 0em' }} vertical>
           <Grid container columns={3}>
             <Grid.Row >
               <Grid.Column>
-              <Link to='./design'><Button size='huge' fluid color='yellow'>DESIGN</Button></Link>
+              <Link to='./design'><Button size='huge' fluid color='yellow'><Icon name='compass'/>DESIGN</Button></Link>
               </Grid.Column>
               <Grid.Column >
-              <Link to='./build'><Button size='huge'fluid color='olive'>BUILD</Button></Link>
+              <Link to='./build'><Button size='huge'fluid color='olive'><Icon name='magic'/>BUILD</Button></Link>
               </Grid.Column>
               <Grid.Column >
-              <Link to='./teach'><Button size='huge'fluid color='green'>TEACHING</Button></Link>
+              <Link to='./teach'><Button size='huge'fluid color='green'><Icon name='student'/>TEACHING</Button></Link>
               </Grid.Column>
             </Grid.Row>
           </Grid>
